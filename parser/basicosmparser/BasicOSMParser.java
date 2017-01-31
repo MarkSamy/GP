4b4b36e7bbfdc2bc46317b7b4a3ef646a97ec719
omar-salah-elboredy <omarfoe94@gmail.com>
2017-01-31 17:24:23 +0200

    Update BasicOSMParser.java

--- parser/basicosmparser/BasicOSMParser.java
+++ parser/basicosmparser/BasicOSMParser.java
@@ -1,12 +1,8 @@
 package basicosmparser;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import org.xml.sax.SAXException;
 
 import controller.*;
@@ -31,74 +27,12 @@ public class BasicOSMParser {
 		}
 	}
 
-	private void CreateHashTable() {
-		HashTable ht = new HashTable();
-		long start1 = System.nanoTime();
-		ht.constructTableMain();
-		long time1 = System.nanoTime() - start1;
-		System.out.println("[INFO] Exported hashtable ................... SUCCESS ["+ (time1 / 1e9) +"]");
-	}
-
-	private void AdjacencyList(int maxLenTable, int maxLenEdges) throws IOException {
-		AdjacencyListBuilder_OLD alb = new AdjacencyListBuilder_OLD();
-		long start1 = System.nanoTime();
-		alb.AdjacencyListBuilderMain(maxLenTable, maxLenEdges);
-		long time1 = System.nanoTime() - start1;
-		System.out.println("[INFO] Exported adjacency list ................... SUCCESS ["+ (time1 / 1e9) +"]");
-	}
-
 	private void DoZOrder() throws IOException {
 		Curve c = new Curve();
 		long start1 = System.nanoTime();
 		c.CurveMain();
 		long time1 = System.nanoTime() - start1;
 		System.out.println("[INFO] Exported ZOrder ................... SUCCESS ["+ (time1 / 1e9) +"]");
-	}
-
-	private int Edit(int caller) throws IOException {
-		PrintWriter writer;
-		File sorted;
-		if (caller == 0) {
-			writer = new PrintWriter(new FileWriter("sortedEdgesEdited.txt"));
-			sorted = new File("sortedEdges.txt");
-		} else {
-			writer = new PrintWriter(new FileWriter("tableEdited.txt"));
-			sorted = new File("table.txt");
-		}
-		BufferedReader br = new BufferedReader(new FileReader(sorted));
-		int maxLen = 0;
-		long start1 = System.nanoTime();
-		String myString = br.readLine();
-		while (myString != null) {
-			if (myString.length() > maxLen) {
-				maxLen = myString.length();
-			}
-			myString = br.readLine();
-		}
-		int noLines = 0;
-		br.close();
-		br = new BufferedReader(new FileReader(sorted));
-		myString = br.readLine();
-		while (myString != null) {
-			while (myString.length() < maxLen) {
-				myString += " ";
-			}
-			writer.println(myString);
-			noLines++;
-			if (noLines % 100000 == 0) {
-				writer.flush();
-			}
-			myString = br.readLine();
-		}
-		br.close();
-		writer.close();
-		long time1 = System.nanoTime() - start1;
-		if (caller == 0) {
-			System.out.println("[INFO] Edited edges ................... SUCCESS ["+ (time1 / 1e9) +"]");
-		} else {
-			System.out.println("[INFO] Edited hash table ................... SUCCESS ["+ (time1 / 1e9) +"]");
-		}
-		return maxLen;
 	}
 
 	private void SortEdges() throws IOException {
