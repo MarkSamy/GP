5e01cf3885b486c3f48a09ade9315d03c1f39714
omar-salah-elboredy <omarfoe94@gmail.com>
2017-03-17 03:26:54 +0200

    Add files via upload

---
+++ parser/model/Files.java
@@ -0,0 +1,26 @@
+package model;
+
+public abstract class Files {
+	
+	public static final String NY_FILE = "new-york-latest.osm";
+	public static final String EG_FILE = "egypt-latest.osm";
+	public static final String MALDIVES_FILE = "maldives-latest.osm";
+	public static final String NODES_FILE = "nodes.txt";
+	public static final String NODES_LON_LAT = "nodesv2.txt";
+	public static final String WAYS_FILE = "ways.txt";
+	public static final String RELS_FILE = "rels.txt";
+	public static final String EDGES_FILE = "edges.txt";
+	public static final String DATA_FILE = "data.txt";
+	public static final String ADJACENCY_LIST = "list.txt";
+	public static final String SORTED_ADJACENCY_LIST = "sortedlistTP.txt";
+	public static final String SORTED_EDGES = "sortedEdges.txt";
+	public static final String ZORDER_FILE = "ZOrder.txt";
+	public static final String SMALL_GRAPH = "smallgraph.txt";
+	public static final String SUPER_GRAPH_INTERNAL = "SuperGraphInternalAdjacencyList.txt";
+	public static final String SUPER_GRAPH_EXTERNAL = "SuperGraphExternalAdjacencyList.txt";
+	public static final String ADJACENCY_LIST_OFFSETS = "listTable.txt";
+	public static final String EDITED_ADJACENCY_LIST_OFFSETS = "listTableEdited.txt";
+	public static final String JOIN_FILE = "Join.txt";
+	public static final String WEIGHTED_EDGES_FILE = "weightedEdges.txt";
+	public static final String WEIGHTED_EDGES_FILE_SORTED = "sortedweightedEdges.txt";
+}
